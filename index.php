c8e4305660ebb4753f64133a2d9cb4b76bc5c840
TyomBen <artyombenikyan@mail.ru>
2023-07-05 21:01:38 +0400

    finish

---
+++ index.php
@@ -0,0 +1,70 @@
+<link rel = "stylesheet" href ="styles.css" >
+<!--1-->
+
+<form method = "post" action = "<?php echo $_SERVER["PHP_SELF"] ?>">
+   Name: <input type = "text" name = "name" /> <br><br>
+   Email: <input type = "email" name = "email" /> <br><br>
+    <input type = "submit" />
+</form>
+
+<?php 
+
+    if (isset ($_POST["name"])){
+        $name = $_POST["name"];
+        echo "Hi $name! ";
+    }
+    if (isset ($_POST["email"])){
+        $email = $_POST ["email"];
+        echo "Its Your $email";
+    }
+?>
+
+<!-- 2 -->
+
+<!-- <?php
+include "./validation&security.php";
+$arrayMonhths = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+$currentYear = date ("Y");
+?>
+<form method = "post" action = "<?php echo htmlspecialchars ($_SERVER["PHP_SELF"]) ?>">
+    <?php echo $nameErr ?>
+    <input type="text" name="name" placeholder = "First Name" value="<?php echo isset($name) ? ($name) : ''; ?>" />
+    <?php echo $lastNameErr ?>
+    <input type = "text" name = "lastname" placeholder = "Last name" value = "<?php echo isset($lastName) ? ($lastName) : ''; ?>" /><br><br>
+    <?php echo $emailErr ?>
+    <input type = "email" name = "email" placeholder = "email" value = "<?php echo isset($email) ? ($email) : ''; ?>" /><br><br>
+    <?php echo $telErr ?>
+    <input type = "tel" name = "tel" placeholder = "Mobile Number" value = "<?php echo isset($tel) ? ($tel) : ''; ?>" /> <br><br>
+    <?php echo $passwordErr ?>
+    <input type = "password" name = "password" placeholder = "New password" value = "<?php echo isset($password) ? ($password) : ''; ?>" /><br><br>
+ 
+     <small class = "default-style">Birthday</small><br>
+    <select>
+        <?php 
+        array_map (function ($months) {
+            echo '<option>' . $months;
+        },$arrayMonhths);
+        ?>
+        </select>
+         <select>
+            <?php
+            for ($i = 1; $i <= 31; $i++){
+                echo "<option>" . $i;
+            }
+            ?>
+        </select> 
+        <select>
+        <?php 
+        for (;$currentYear >= 1905; $currentYear--){
+            echo "<option>" . $currentYear;
+        }
+        ?>
+        </select> <br><br>
+         <small class = "default-style">Gender</small>
+        <?php echo $genderErr . "<br>" ?>
+        <label for = "female"><small class = "default-style"> Female </small><input type = "radio" name = "gender" id = "female" value = "female" <?php if (isset ($gender) && $gender == 'female') $genderErr = '' ?> /> 
+        <label for = "male"><small class = "default-style"> Male </samll><input type = "radio" name = "gender" id = "male" value = "male" <?php if (isset ($gender) && $gender == 'male') $genderErr = ''  ?> /> <br><br>
+        <button> Sign Up </button>  
+</form> -->
+
+
---
+++ validation&security.php
@@ -0,0 +1,64 @@
+<link rel = "stylesheet" href ="styles.css" >
+<?php
+$name = $lastName = $email = $tel = $password = $gender = "";
+$nameErr = $lastNameErr = $emailErr = $telErr = $passwordErr = $genderErr = "";
+$phoneRegex = "/^(\+374(3|5|7|9))\d{7}$/";
+$nameRegex = "/^[a-zA-z]*$/";
+$flag = false;
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+   if (empty ($_POST["name"])) {
+    $nameErr = '<small class = required-error> Name is required </small>';
+   }else {
+    $name = clear_input ($_POST["name"]);
+    if (!preg_match ($nameRegex, $name)) {
+    $nameErr = "<small class = required-error> Write Your Name </small>";
+    }
+   }
+   if (empty ($_POST["lastname"])){
+    $lastNameErr = "<small class = required-error> Lastname is required </small>";
+   }else {
+    $lastName = clear_input ($_POST["lastname"]);
+    if (!preg_match ($nameRegex, $lastName)){
+        $lastNameErr= "<small class = required-error> Write Your Lastname </small>";
+    }
+   }
+   if (empty ($_POST["email"])){
+    $emailErr ="<small class = required-error> Email is required </small>";
+   }else {
+    $email = clear_input ($_POST["email"]);
+    if (filter_var ($email, FILTER_VALIDATE_EMAIL)) {
+        $email = clear_input ($_POST["email"]);
+    }else {
+        $emailErr = "<small class = required-error> Write Your Valid Email </small>";
+    }
+   }
+   if (empty($_POST['tel'])) {
+    $telErr = "<small class = required-error> Telephone is required </small>";
+   }else {
+    $tel = clear_input ($_POST["tel"]); 
+    if (!preg_match ($phoneRegex, $tel)){
+    $telErr = "<small class = required-error> Write Your Tel </small>";
+    }
+   }
+   if (empty ($_POST["password"])){
+    $passwordErr = "<small class = required-error> Password is required";
+   }else {
+    $password = clear_input ($_POST["password"]);
+   }
+   if (empty ($_POST['gender'])){
+    $genderErr = "<small class = required-error> Gender is required";
+   }else {
+    $gender = clear_input ($_POST["gender"]);
+   }
+}
+
+function clear_input ($val) {
+    $val = trim ($val);
+    $val = strip_tags ($val);
+    $val = stripslashes ($val);
+    $val = htmlspecialchars ($val);
+    return $val;
+}
+?>
+
